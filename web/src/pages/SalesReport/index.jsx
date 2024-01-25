e43282552bc355c32c417bfa93269546419c24e0
Natalye <cavalieridev@gmail.com>
2024-01-25 15:44:20 +0100

    rest

--- web/src/pages/SalesReport/index.jsx
+++ web/src/pages/SalesReport/index.jsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
+import {api}  from '../../services/api.js'
 
 export function SalesReport() {
+  useEffect(()=>{
+    api
+    .get("/sales", {withCredentials: true})
+    .then((response)=> console.log(response.data))
+  },[])
   return (
     <Container>
       <h1>Relatório de Vendas</h1>
--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -7,9 +7,22 @@ import { CostumerRoutes } from "./costumer.routes"
 import { SaleRoutes } from "./sale.routes"
 import { AuthRoutes } from './auth.routes';
 import { USER_ROLES } from "../utils/roles"
+import { useEffect } from 'react';
+import { api } from '../services/api';
+
 
 export function Routes() {
-  const { user } = useAuth();
+  const { user, signOut } = useAuth();
+
+  useEffect(()=> {
+    api.get("/users/validated").catch((error)=>{
+     { 
+      if(error.response?.status === 401){
+        signOut()
+      }
+    }
+    })
+  }, [])
 
     function AccessRoutes(){
       switch(user.role){
